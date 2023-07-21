b34642c66271572196aac9c8d03199861a501c56
Aleksey Burn <41241809+alekseyburn@users.noreply.github.com>
2023-07-22 00:42:09 +0700

    init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,31 +1,31 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  experimental: { inlineSSRStyles: false },
+  experimental: { inlineSSRStyles: false, payloadExtraction: false },
   css: ['@/assets/scss/common.scss'],
   vite: {
     css: {
       preprocessorOptions: {
         scss: {
-          additionalData: '@use "@/assets/scss/_variables.scss" as *;'
-        }
-      }
-    }
+          additionalData: '@use "@/assets/scss/_variables.scss" as *;',
+        },
+      },
+    },
   },
   app: {
     head: {
       viewport: 'width=device-width,initial-scale=1',
       link: [
-        {rel: 'preconnect', href: 'https://fonts.googleapis.com'},
-        {rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: ""},
+        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+        { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
         {
           rel: 'stylesheet',
-          href: 'https://fonts.googleapis.com/css2?family=Poppins:wght@500&family=Ubuntu:wght@400;500;700&display=swap'
+          href: 'https://fonts.googleapis.com/css2?family=Poppins:wght@500&family=Ubuntu:wght@400;500;700&display=swap',
         },
 
       ],
       meta: [
-        {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       ],
     },
-  }
+  },
 })
